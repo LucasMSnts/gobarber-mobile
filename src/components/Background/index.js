248d646f8231d531892b18d78fe8765662208f5d
Lucas Mauro <lucas_de_mauro@hotmail.com>
2019-11-27 15:09:44 -0300

    Configurando background

---
+++ src/components/Background/index.js
@@ -0,0 +1,11 @@
+import LinearGradient from 'react-native-linear-gradient';
+import styled from 'styled-components/native';
+
+export default styled(LinearGradient).attrs({
+    colors: ['#7159c1', '#ab59c1'],
+})`
+    flex: 1;
+`;
+// No momento, precisa linkar
+// react-native link react-native-linear-gradient
+// react-native run-android
